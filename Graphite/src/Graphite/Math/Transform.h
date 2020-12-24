1077a7893f62a323f65445a8b9608a0c7c81bc29
Jan Kresic <jan.kresic@fer.hr>
2020-12-24 17:51:03 +0100

    Math merge and transform fix and cleanup

--- Graphite/src/Graphite/Math/Transform.h
+++ Graphite/src/Graphite/Math/Transform.h
@@ -1,39 +1,51 @@
 #pragma once
+
+#ifndef GRAPHITE_TRANSFORM_H
+#define GRAPHITE_TRANSFORM_H
+
 #include "Graphite/Core/grpch.h"
+#include "Graphite/Core/Core.h"
 #include <glm/glm.hpp>
 #include <glm/gtx/quaternion.hpp>
+
 namespace Graphite
 {
 	namespace Math
 	{
-		class Transform
-		{
-		private:
-			glm::vec3 position;
-			glm::quat rotation;
-			glm::vec3 size;
 
-		public:
-			Transform(glm::vec3 position = glm::vec3(0, 0, 0), glm::quat rotation = glm::quat(1, 0, 0, 0), float size = 1);
-
-			void rotate(glm::vec3& vec, float angle);
-			void rotate(std::vector<Rotation>& rotations);
-			void rotate(glm::quat& quaternion);
-			void rotate(std::vector<glm::quat>& quaternions);
-
-			void translate(glm::vec3& translation);
-
-			void setPosition(glm::vec3 position);
-
-			glm::vec3 getPosition();
-
-			void setSize(glm::vec3 size);
-
-			glm::mat4 getMatrix();
-		}
 		struct Rotation {
 			glm::vec3 vec;
 			float angle;
 		};
+		
+		class GRAPHITE_API Transform
+		{
+		public:
+			Transform(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::quat rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+			virtual ~Transform() = default;
+			
+			void Rotate(glm::vec3& vec, float angle);
+			void Rotate(std::vector<Rotation>& rotations);
+			void Rotate(glm::quat& quaternion);
+			void Rotate(std::vector<glm::quat>& quaternions);
+
+			inline void Translate(glm::vec3& translation) { m_Position = translation; }
+
+			inline glm::vec3 GetPosition() const { return m_Position; }
+			inline glm::vec3 GetSize() const { return m_Size; }
+
+			inline void SetSize(glm::vec3 size) { m_Size = size; }
+
+			glm::mat4 GetModelMatrix() const;
+
+		private:
+			glm::vec3 m_Position;
+			glm::quat m_Rotation;
+			glm::vec3 m_Size;
+
+		};
+		
 	}
 }
+
+#endif
